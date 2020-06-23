790781ee1788dcca2d7df1478efd1b4dd1065046
RichardMEN11 <richard.menning@stud.hs-ruhrwest.de>
2020-06-23 17:09:07 +0200

    finish test project

---
+++ README.md
@@ -0,0 +1,14 @@
+# Simple Test of Directus and Gridsome
+
+This project is simply for testing gridsome and directus cms.
+
+(Directus CMS) [https://directus.io/](Gridsome) [https://gridsome.org/]
+
+## Take of
+
+```
+ docker-compose up -d
+ cd directus-test
+ yarn
+ gridsome develop
+```
---
+++ directus-test/src/pages/Index.vue
@@ -0,0 +1,51 @@
+<template>
+  <Layout>
+    <!-- Learn how to use images here: https://gridsome.org/docs/images -->
+    <g-image alt="Example image" src="~/favicon.png" width="135" />
+
+    <h1>{{ $page.allTest.edges[0].node.test_text }}</h1>
+
+    <p>
+      {{ $page.allTest.edges[0].node.next }}
+    </p>
+
+    <p class="home-links">
+      <a href="https://gridsome.org/docs/" target="_blank" rel="noopener"
+        >Gridsome Docs</a
+      >
+      <a
+        href="https://github.com/gridsome/gridsome"
+        target="_blank"
+        rel="noopener"
+        >GitHub</a
+      >
+    </p>
+  </Layout>
+</template>
+
+<page-query>
+  query {
+  allTest {
+    edges {
+      node {
+        test_text
+        next
+      }
+    }
+  }
+}
+</page-query>
+
+<script>
+export default {
+  metaInfo: {
+    title: "Hello, world!",
+  },
+};
+</script>
+
+<style>
+.home-links a {
+  margin-right: 1rem;
+}
+</style>
